4e6b33cbb96d7bd19df37a7de798684bf2b81267
Devloper.JS <jspark6815@daum.net>
2018-11-22 02:20:21 +0900

    Commit login

---
+++ frontend-master/js/main.js
@@ -0,0 +1,7 @@
+setTimeout(function() { 
+    var splash = document.getElementById('container1');
+    splash.style.display = 'none'; 
+    var mainer = document.getElementById('container2');
+    mainer.style.display = 'flex';
+    console.log('check');
+ }, 2000);
