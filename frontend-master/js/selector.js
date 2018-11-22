35670d75d8a11ee407955d8f79df121fb8993e64
Devloper.JS <jspark6815@daum.net>
2018-11-23 00:07:03 +0900

    Add start

---
+++ frontend-master/js/selector.js
@@ -0,0 +1,17 @@
+function first_conf() {
+    var code = document.getElementById('code').value;
+    if(code == '247651-518924')
+    {
+        document.getElementById('submit').style.display = "none";
+        document.getElementById('basil_content').style.display = "flex";
+        document.getElementById('nickname').style.display = "flex";
+    }
+    else {
+        if(code == '') {
+            alert("제품 코드를 입력하세요!");
+        }
+        else {
+            alert("알맞은 코드가 아닙니다ㅜㅜ")
+        }
+    }
+}
